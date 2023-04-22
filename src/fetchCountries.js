f2dd9ab770d671f5f7fd6aee5fc6772384db6119
GlobalSatUA <alex.trenbach@gmail.com>
2023-04-22 15:01:45 +0300

    second

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,21 +1,28 @@
 export const fetchCountries = (name) => {
-    const BASE_URL = `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`;
-  
-    return fetch(BASE_URL)
-      .then((response) => {
-        if (response.ok) {
-          return response.json();
-        }
-        throw new Error('Oops, there is no country with that name');
-      })
-      .then((data) => {
-        // Make sure languages is always an array
-        data.forEach((country) => {
-          if (!Array.isArray(country.languages)) {
-            country.languages = [country.languages];
+  const BASE_URL = `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`;
+
+  return fetch(BASE_URL)
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error('Oops, there is no country with that name');
+    })
+    .then((data) => {
+      data.forEach((country) => {
+        
+        if (!Array.isArray(country.languages)) {
+          const langArray = [];
+          for (const [key, value] of Object.entries(country.languages)) {
+            langArray.push({ name: value });
           }
-        });
-        return data;
+          country.languages = langArray;
+        }
+        
+        if (country.languages.length === 0) {
+          country.languages = [{ name: 'Unknown' }];
+        }
       });
-  };
-  
+      return data;
+    });
+};
